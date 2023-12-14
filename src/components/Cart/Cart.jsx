31ee7ce3fcbffaebd948b9b54319d32853bf325b
Raihanroo <raihanroo21@gmail.com>
2023-12-14 12:27:30 +0300

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,9 +9,20 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
+
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+    /**simple solution by using if 
+     * 
+     * if(priduct.quantity === 0){
+     *   poduct .quantity = 1;
+     * }
+     */
+        product.quantity = product.quantity || 1;    /**short-cut solution */
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice*7/100;
@@ -21,7 +32,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
              <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+                <p>Selected Items: {quantity}</p>
                 <p>Total Price: ${totalPrice}</p>
                 <p>Total Shipping: ${totalShipping}</p>
                 <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -38,6 +38,8 @@ useEffect(() =>{
 
     const handleAddToCart = (product) =>{
         const newCart = [...cart, product];
+
+        
          setCart(newCart);
          addToDb(product.id)
        }
@@ -64,3 +66,24 @@ export default Shop;
 
 
 
+ /*********The path Of **Hard Solution by following Cart.jsx */
+    
+         // if priduct doest exist in the cart , then set quantity = 1
+        // if exist update quantity by 1
+
+        /*let newCart = []/
+    
+        /**
+         * const exits = cart.find(pd => pd.id === product.id);
+         * if(!exists){
+         * product.quantity = 1;
+         * newCart = [...cart, product]
+         * 
+         * }
+         * else{
+         *   exists.quantity = exists.quantity + 1;
+         *   const remaining = cart.filter(pd => pd.id !== product.id);
+         *   newCart = [...remaining, exists]
+         * }
+         * 
+         * */
